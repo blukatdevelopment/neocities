f55b1480d7076bd3643f435de2c463672527971f
blukat <blukatstudios@gmail.com>
2024-07-05 10:15:33 -0400

    Updated asset links to use github.

--- games/blaster/main.js
+++ games/blaster/main.js
@@ -67,9 +67,9 @@ function isInsideBox(topLeft, bottomRight, point){
 /*##############################################################################
 # SFX
 ##############################################################################*/
-const SFX_PEW = "https://cdn.discordapp.com/attachments/1255876329002504193/1256001477525373029/pew.mp3?ex=667f2e07&is=667ddc87&hm=9fd59691868655e0a67d884196f675635350d2751da4085f101b8ae1204d7093&",
-SFX_IMPACT = "https://cdn.discordapp.com/attachments/1255876329002504193/1256001477277777941/impact.mp3?ex=667f2e07&is=667ddc87&hm=16fac53ce143b27ebec950fd53911f0ccf7cb6294b1535c15a69afe1f07de913&",
-SFX_EXPLOSION = "https://cdn.discordapp.com/attachments/1255876329002504193/1256001477034381394/explosion.mp3?ex=667f2e07&is=667ddc87&hm=b7b0793a36552953c175bf410f624bd3ef0d2feb603a66ee2e87d364e53c7fcd&";
+const SFX_PEW = "https://github.com/blukatdevelopment/neocities/raw/main/assets/pew.mp3",
+SFX_IMPACT = "https://github.com/blukatdevelopment/neocities/raw/main/assets/impact.mp3",
+SFX_EXPLOSION = "https://github.com/blukatdevelopment/neocities/raw/main/assets/explosion.mp3";
 
 function playSound(file){
   var audio = new Audio(file);
